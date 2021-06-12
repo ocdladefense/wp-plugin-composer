c5080696888595d048b0a200aeaa3023534cf973
Jose Bernal <jbernal.web.dev@gmail.com>
2021-06-12 14:57:05 -0700

    Add required WordPress plugin metadata.

--- wp-composer.php
+++ wp-composer.php
@@ -1,3 +1,19 @@
 <?php
 
-require __DIR__ . "/vendor/autoload.php";
+
+/**
+ * @package ComposerPlugin
+ */
+
+/*
+Plugin Name: Composer Plugin
+Plugin URI: 
+Description: Include the Composer autoloader.
+Version: 1.0.0
+Author: LTD Global
+Author URI: https://www.ocdla.org/
+License: GPLv2 or later
+Text Domain: wp-composer-plugin
+*/
+
+require ABSPATH . "vendor/autoload.php";
